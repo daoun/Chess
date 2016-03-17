aeecc3fcce15fc8dfc965a687a26e1a712e7497b
Capki Kim <max930810@gmail.com>
2016-03-17 00:42:48 -0400

    King done partially

--- Chess38/src/chess/Knight.java
+++ Chess38/src/chess/Knight.java
@@ -18,37 +18,39 @@ public class Knight extends Piece {
 		int rank = this.getRank();
 		int file = this.getFile();
 		
-		if(!(rank - 1 < 0 || file - 2 < 0)){
-			this.addPosition(rank - 1, file - 2, posList);
+		if(!(rank - 1 < 0 || file - 1 < 0)){
+			this.addPosition(rank - 1, file - 1, posList);
 		}
 		
-		if(!(rank - 2 < 0 || file - 1 < 0)){
-			this.addPosition(rank - 2, file - 1, posList);
+		if(!(rank - 1 < 0)){
+			this.addPosition(rank - 1, file, posList);
 		}
 		
-		if(!(rank - 2 < 0 || file + 1 > 7)){
+		if(!(rank - 1 < 0 || file + 1 > 7)){
 			this.addPosition(rank - 2, file + 1, posList);
 		}
 		
-		if(!(rank - 1 < 0 || file + 2 > 7)){
-			this.addPosition(rank - 1, file + 2, posList);
+		if(!(file - 1 < 0)){
+			this.addPosition(rank, file - 1, posList);
 		}
 		
-		if(!(rank + 1 > 7 || file + 2 > 7)){
-			this.addPosition(rank + 1, file + 2, posList);
+		if(!(file + 1 > 7)){
+			this.addPosition(rank, file + 1, posList);
 		}
 		
-		if(!(rank + 2 > 7 || file + 1 > 7)){
-			this.addPosition(rank + 2, file + 1, posList);
+		if(!(rank + 1 > 7 || file - 1 < 0)){
+			this.addPosition(rank + 1, file - 1, posList);
 		}
 		
-		if(!(rank + 2 > 7 || file - 1 < 0)){
-			this.addPosition(rank + 2, file - 1, posList);
+		if(!(rank + 1 > 7)){
+			this.addPosition(rank + 1, file, posList);
 		}
 		
-		if(!(rank + 1 > 7 || file - 2 < 0)){
-			this.addPosition(rank + 1, file - 2, posList);
+		if(!(rank + 1 > 7 || file + 1 > 7)){
+			this.addPosition(rank + 1, file + 1, posList);
 		}
+		
+		//Castling
 		
 		return posList;
 	}
